ea728e86a44e0c25fa927f7cb17659d5a8f0706c
Shadoww-nx <62445534+Shadoww-nx@users.noreply.github.com>
2020-03-20 22:41:20 +0300

    Create Neuronet.hpp

---
+++ Neuronet.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Neuronet{
+    public:
+        Neuronet(int, double);
+        string training_neuronet(bool, int, string, int);
+        ~Neuronet();
+    private:
+        string training_neuronet_full(int, string);
+        string training_neuronet_min(int, string);
+        string boolean_function(int);
+        void truth_table_create(int);
+        unsigned long long fact(int);
+        void combination(int**, int, int);
+        unsigned int number_of_variables = 0;
+        unsigned int number_of_lines = 0;
+        int** truth_table = nullptr;
+        int* Y = nullptr;
+        double nu = 0;
+};
